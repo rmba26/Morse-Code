0e06c8bd896f7ae4f4a1924529a2b64389521d18
rmba26 <124733772+rmba26@users.noreply.github.com>
2023-04-28 22:09:09 -0400

    Add files via upload

---
+++ Morse Decoder.playground/Contents.swift
@@ -0,0 +1,114 @@
+// Decrypting morse code message:
+
+let morseCodeDecrypt = [
+  
+".-": "a",
+"-...": "b",
+"-.-.": "c",
+"-..": "d",
+".": "e",
+"..-.": "f",
+"--.": "g",
+"....": "h",
+"..": "i",
+".---": "j",
+"-.-": "k",
+".-..": "l",
+"--": "m",
+"-.": "n",
+"---": "o",
+".--.": "p",
+"--.-": "q",
+".-.": "r",
+"...": "s",
+"-": "t",
+"..-": "u",
+"...-": "v",
+".--": "w",
+"-..-": "x",
+"-.--": "y",
+"--..": "z"
+
+]
+
+var code = ".-. --- -. .- .-.. -..   -... .- .-. .- --. .-   .- .-. ... ..- .- --. .-"
+
+code += " "
+
+var letter: String = ""
+var charac = ""
+var counter = 0
+
+
+for item in code {
+  
+  if item != " " {
+    letter.append(item)
+    counter = 0
+  continue
+     
+} else {
+    counter += 1
+    if counter == 3 {
+     charac.append(" ")
+      
+      
+    }
+    }
+    if let alpha = morseCodeDecrypt["\(letter)"] {
+      charac.append(alpha)
+      letter = ""
+      
+      
+    } else {
+      //print("Letter not found")
+   }
+
+}
+print(charac)
+
+// Encrypting text into morse code
+
+let morseCodeEncrypt = [
+  "a": ".-",
+  "b": "-...",
+  "c": "-.-.",
+  "d": "-..",
+  "e": ".",
+  "f": "..-.",
+  "g": "--.",
+  "h": "....",
+  "i": "..",
+  "j": ".---",
+  "k": "-.-",
+  "l": ".-..",
+  "m": "--",
+  "n": "-.",
+  "o": "---",
+  "p": ".--.",
+  "q": "--.-",
+  "r": ".-.",
+  "s": "...",
+  "t": "-",
+  "u": "..-",
+  "v": "...-",
+  "w": ".--",
+  "x": "-..-",
+  "y": "-.--",
+  "z": "--..",
+]
+
+let textMessage = "ronald baraga arsuaga"
+
+var morseCode = ""
+
+for char in textMessage {
+  if char == " "{
+  morseCode.append("   ")
+} else if let alpha = morseCodeEncrypt["\(char)"] {
+      morseCode.append(alpha + " ")
+}
+}
+
+print(morseCode)
+    
